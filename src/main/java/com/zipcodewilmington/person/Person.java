1c82f10b7f08b9d174473ff098417aa39aa23ba4
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-07-07 09:09:19 -0400

    completed and tested the person class

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -8,28 +8,38 @@ public class Person {
     private int age;
 
     public Person() {
+        setAge(Integer.MAX_VALUE);
+        setName("");
     }
 
     public Person(int age) {
+        this.age = age;
+        setName("");
     }
 
     public Person(String name) {
+        setAge(Integer.MAX_VALUE);
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
